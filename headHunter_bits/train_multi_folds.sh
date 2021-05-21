fbc63a31bf077732ac52f369b31d3023cfffe52f
edhendo <edhendo@btinternet.com>
2021-05-21 16:18:22 +0100

    w'end run

--- headHunter_bits/train_multi_folds.sh
+++ headHunter_bits/train_multi_folds.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-folds='2'
+folds='1 2 3 4 5'
 for fold_num in $folds
 do
-    CUDA_VISIBLE_DEVICES='1' python3 train_headHunter.py --fold_num $fold_num --GPUs 1 
+    CUDA_VISIBLE_DEVICES='0' python3 train_headHunter.py --fold_num $fold_num --GPUs 1
 done
