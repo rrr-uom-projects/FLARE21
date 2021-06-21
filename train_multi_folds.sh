83544997377b3ec4972457f0840bf40f6efd2a75
edhendo <edhendo@btinternet.com>
2021-06-21 11:27:57 +0100

    move this

---
+++ train_multi_folds.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sleep 6h
+CUDA_VISIBLE_DEVICES='0' python3 train3.py --fold_num 1 --GPUs 1
+sleep 10m
+CUDA_VISIBLE_DEVICES='0' python3 train2.py --fold_num 1 --GPUs 1
+folds='2 3 4 5'
+for fold_num in $folds
+do
+    sleep 10m
+    CUDA_VISIBLE_DEVICES='0' python3 train.py --fold_num $fold_num --GPUs 1
+done
