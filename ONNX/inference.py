ef3c2affb6329cc862a43b14198c6bf12a432dab
DMcSweeney <donalmcs1@gmail.com>
2021-07-19 16:30:10 +0100

    Fix issue with tiling inputs

--- ONNX/inference.py
+++ ONNX/inference.py
@@ -2,6 +2,7 @@
 ONNX model inference
 
 """
+import albumentations as A
 import os
 import torch
 import sys
@@ -11,13 +12,11 @@ import onnxruntime as ort
 import time
 from multiprocessing import Pool
 from argparse import ArgumentParser
-
+import matplotlib.pyplot as plt
 
 from torch.utils.data import Dataset, DataLoader
-from torchvision.transforms import Compose
+from albumentations.pytorch.transforms import ToTensor
 
-
-from ViT.utils.transforms import PrepareForNet
 from roughSeg.utils import k_fold_split_train_val_test, getFiles
 
 
@@ -31,7 +30,7 @@ parser.add_argument("model_path", help="Path to ONNX model", type=str)
 parser.add_argument("outsize_path", help="Path to CSV file with output sizes", type=str)
 args = parser.parse_args()
 
-test_workers = 8
+test_workers = 1
 batch_size = 1
 
 
@@ -40,11 +39,10 @@ class customDataset(Dataset):
     def __init__(self, image_path, out_size_file, transforms, indices, apply_WL, window=[400,100], level=[50,60]):
         self.indices = indices
         self.image_path = image_path
-        self.out_size = np.load(out_size_file)
+        self.out_size = np.load(out_size_file)[indices]
         self.organ_to_idx = ["Background", "Liver", "Kidney", "Spleen", "Pancreas"]
         self.names = self.idx_to_names(image_path)
-        self.availableImages = [sorted(getFiles(image_path))[
-            ind] for ind in indices]
+        self.availableImages = [sorted(getFiles(image_path))[ind] for ind in indices]
         #self.availableImages.remove('train_079.npy')
         self.transforms = transforms
         self.window = window
@@ -111,13 +109,15 @@ class customDataset(Dataset):
                 arr = []
                 for i in range(len(self.window)):
                     arr.append(self.WL_norm(img, self.window[i], self.level[i]))
-                img = np.array(arr).reshape(*img.shape, len(self.window))
+                img = np.stack(arr, axis=-1)
             else:
                 img = self.WL_norm(img, self.window, self.level)[..., np.newaxis]
         else:
             img = img[..., np.newaxis]
         if self.transforms:
-            augmented = self.transforms({"image": img})
+            print(img.shape)
+            augmented = self.transforms(image=img)
+            print(augmented["image"].shape)
             sample = {'inputs': augmented["image"],
                       'out_size': (self.out_size[index], 512, 512),
                       'id': pid}
@@ -134,18 +134,16 @@ def to_numpy(x):
     return x.detach().cpu().numpy() if x.requires_grad else x.cpu().numpy()
 
 def main():
-    test_transforms = Compose([
-        PrepareForNet(), #*Channels first
+    test_transforms = A.Compose([
+        ToTensor(), #*Channels first
     ])
 
     dataset_size = len(getFiles(img_dir))
     _, _, test_idx = k_fold_split_train_val_test(
         dataset_size, fold_num=1, seed=230597) #! Use test set from first fold for now
-    test_dataset = customDataset(args.img_dir, args.outsize_path, test_transforms, indices=test_idx, apply_WL=True)
+    test_dataset = customDataset(args.img_dir, args.outsize_path, test_transforms, window=[400, 100], level=[100, 50], indices=test_idx, apply_WL=True)
 
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, pin_memory=False,
-                             num_workers=test_workers, worker_init_fn=lambda _: np.random.seed(
-                                 int(torch.initial_seed()) % (2**32-1)))
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
     
     print("ONNX Available providers:", ort.get_available_providers())
     print(ort.get_device())
@@ -154,7 +152,7 @@ def main():
     sess_options.graph_optimization_level = ort.GraphOptimizationLevel.ORT_ENABLE_ALL
     sess_options.execution_mode = ort.ExecutionMode.ORT_SEQUENTIAL  # ! or ORT_SEQUENTIAL
     sess_options.optimized_model_filepath = "./optimized_model.onnx"
-    sess_options.log_severity_level = 1
+    sess_options.log_severity_level = 0
     sess_options.enable_profiling = False
     sess_options.inter_op_num_threads = os.cpu_count() - 1 
     sess_options.intra_op_num_threads = os.cpu_count() - 1
@@ -164,15 +162,24 @@ def main():
     t = time.time()
     output_dict = {}
     for data in test_loader:
+        print(data['id'][0])
         out_size = [int(x) for x in data['out_size']]
         inputs = [data['inputs'], *(torch.tensor(x) for x in out_size)]
+
+        fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+        ax.imshow(inputs[0][0, 0, 45], cmap='gray')
+
+        fig.savefig(f'./outputs/{data["id"][0]}.png')
+
         ort_inputs = {key.name: to_numpy(x) for key, x in zip(ort_session.get_inputs(), inputs)}
         outputs = np.array(ort_session.run(None, ort_inputs))
-        output_dict[data['id'][0]] = sigmoid(outputs)
-        #break
+        output_dict[data['id'][0]] = np.argmax(
+            np.squeeze(outputs), axis=0).astype(np.int8)
+        break
 
     for key, val in output_dict.items():
-        np.save(f'./outputs/{key}', val)
+        np.save(
+            f'/data/FLARE21/models/full_runs/tiny_segmenter_192/fold1/outputs/{key}.npy', val)
     print(f'Execution time: {time.time() - t} for {len(test_idx)} examples.')
 
 if __name__ == '__main__':
