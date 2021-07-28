64e35a70d10c0e91b7d0377cc990e76b86e1e5a1
DMcSweeney <donalmcs1@gmail.com>
2021-07-28 14:32:55 +0100

    Update model weights

--- ONNX/export.sh
+++ ONNX/export.sh
@@ -1,4 +1,4 @@
-python export_model.py /data/FLARE21/models/full_runs/nano_segmenter_192/fold1/ \
+python export_model.py /data/FLARE21/models/full_runs/nano_segmenter_192/fold2/ \
                         compiled_model_nano \
                         --check_model True \
                         --check_output True \
--- ONNX/run_inference.sh
+++ ONNX/run_inference.sh
@@ -1,3 +1,3 @@
 python inference.py /data/FLARE21/training_data_192_sameKidneys/scaled_ims/ \
-                compiled_model.onnx \
+                compiled_model_nano.onnx \
                 /data/FLARE21/original_image_slices.npy
