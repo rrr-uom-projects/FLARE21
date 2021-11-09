6ea44878e2f2868372c147e44469ca740b43938f
edhendo <edhendo@btinternet.com>
2021-11-09 16:06:58 +0000

    ONNX export

--- ONNX/export.sh
+++ ONNX/export.sh
@@ -1,4 +1,4 @@
-python export_model.py /data/FLARE21/models/AbdomenCT-1K/fold1/ \
-                        compiled_model_AbdomenCT1K \
+python export_model.py /data/FLARE21/models/AbdomenCT-1K_tumor/ \
+                        compiled_model_AbdomenCT1K_tumor \
                         --check_model True \
                         --check_output True \
