d44907137fb26d188ac097746b109a40d01b1b30
edhendo <edhendo@btinternet.com>
2021-11-09 16:04:56 +0000

    export to ONNX

--- ONNX/export.sh
+++ ONNX/export.sh
@@ -1,4 +1,4 @@
-python export_model.py /data/FLARE21/models/AbdomenCT-1K/fold1/ \
-                        compiled_model_AbdomenCT1K \
+python export_model.py /data/FLARE21/models/AbdomenCT-1K_12organ/ \
+                        compiled_model_AbdomenCT1K_12organ \
                         --check_model True \
                         --check_output True \
